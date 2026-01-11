de2ee94d08cd115626e07abe202e2496fe7915dc
Prateek Kumar singh <prateekkumarsingh159@gmail.com>
2026-01-11 22:29:27 +0530

    SumOff2No.java
    Removed an unnecessary blank line in the main method.

--- Basic_Program/SumOff2No.java
+++ Basic_Program/SumOff2No.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class SumOff2No {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter 2 no: ");
         int num1=sc.nextInt();
         int num2=sc.nextInt();
