dd7bb867c78b9e73dbdbb79dab0674799cd509b8
Prateek Kumar Singh <prateekkumarsingh159@gmail.com>
2026-04-10 11:49:41 +0530

    class test

---
+++ gla_logistic/RestCheckpoint.java
@@ -0,0 +1,18 @@
+package com.gla_logistic;
+
+
+class RestCheckpoint extends Checkpoint{
+    public RestCheckpoint(String id,String loc,double dist,int exp,int act){
+        super(id, loc, dist, exp, act);
+    }
+    public boolean isCritical(){
+        return false;
+    }
+    public String getType(){
+        return "RestCheckpoint";
+    }
+    public double calculatePenalty(){
+        if (!isDelayed()) return 0;
+        return (actualDuration - expectedDuration)*0.5;
+    }
+}
